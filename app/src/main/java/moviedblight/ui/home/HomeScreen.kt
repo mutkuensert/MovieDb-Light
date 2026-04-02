01e222fb1cc75845daae51e5500c71c03f254723
M. Utku Ensert <mutkuensert@hotmail.com>
2026-04-02 21:15:11 +0300

    fix: configure on ui thread to prevent race condition

--- app/src/main/java/moviedblight/ui/home/HomeScreen.kt
+++ app/src/main/java/moviedblight/ui/home/HomeScreen.kt
@@ -27,8 +27,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -82,9 +82,9 @@ fun HomeScreen(navigator: Navigator) {
 
         Box {
             val navController = rememberNavController()
-
-            LaunchedEffect(navController) {
+            remember(navController) {
                 navigator.configure(navController)
+                true
             }
 
             MainNavigation(
--- core/ui/src/main/java/core/ui/navigation/Navigator.kt
+++ core/ui/src/main/java/core/ui/navigation/Navigator.kt
@@ -4,49 +4,33 @@ import androidx.navigation.NavDestination.Companion.hasRoute
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class Navigator {
     lateinit var controller: NavHostController
         private set
-    private val coroutineScope = CoroutineScope(Job() + Dispatchers.Main)
+
     fun configure(controller: NavHostController) {
         this.controller = controller
     }
 
     inline fun <reified T> navigateToTab(tab: T) {
-        callAfterControllerInit {
-            val isAlreadySelected = controller.currentDestination?.hierarchy
-                ?.any { it.hasRoute(tab::class) } == true
-            controller.navigate(route = tab as Any) {
-                val startDestination = controller.graph.findStartDestination()
-                popUpTo(startDestination.id) {
-                    saveState = !isAlreadySelected
-                }
-                restoreState = !isAlreadySelected
-                launchSingleTop = true
+        val isAlreadySelected = controller.currentDestination?.hierarchy
+            ?.any { it.hasRoute(tab::class) } == true
+        controller.navigate(route = tab as Any) {
+            val startDestination = controller.graph.findStartDestination()
+            popUpTo(startDestination.id) {
+                saveState = !isAlreadySelected
             }
+            restoreState = !isAlreadySelected
+            launchSingleTop = true
         }
     }
 
     fun navigateToRoute(route: Any) {
-        callAfterControllerInit { controller.navigate(route) }
+        controller.navigate(route)
     }
 
     fun navigateBack() {
-        callAfterControllerInit { controller.popBackStack() }
-    }
-
-    fun callAfterControllerInit(block: () -> Unit) {
-        coroutineScope.launch {
-            while (!::controller.isInitialized) {
-                delay(300)
-            }
-            block.invoke()
-        }
+        controller.popBackStack()
     }
 }
