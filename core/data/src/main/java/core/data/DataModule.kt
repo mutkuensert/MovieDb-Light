c17aadb029a39545da55800c811b85ac892518b6
M. Utku Ensert <mutkuensert@hotmail.com>
2026-05-28 19:16:24 +0300

    feat: add timeout for requests

--- core/data/src/main/java/core/data/DataModule.kt
+++ core/data/src/main/java/core/data/DataModule.kt
@@ -24,6 +24,7 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 import utils.stringresource.StringResource
+import java.util.concurrent.TimeUnit
 
 val dataModule = module {
     single { getJson() }
@@ -81,5 +82,6 @@ private fun getClient(
             level = HttpLoggingInterceptor.Level.BODY
         })
         .addInterceptor(ChuckerInterceptor(context))
+        .callTimeout(30000L, TimeUnit.SECONDS)
         .build()
 }
