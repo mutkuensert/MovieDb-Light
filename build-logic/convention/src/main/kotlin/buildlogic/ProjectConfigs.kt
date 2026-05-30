91ae681bfb4528ceda1ef432f67a6649587a0414
M. Utku Ensert <mutkuensert@hotmail.com>
2026-05-30 16:24:36 +0300

    build: update versions

--- build-logic/convention/src/main/kotlin/buildlogic/ProjectConfigs.kt
+++ build-logic/convention/src/main/kotlin/buildlogic/ProjectConfigs.kt
@@ -8,8 +8,8 @@ internal object ProjectConfigs {
     const val COMPILE_SDK = 36
     const val MIN_SDK = 24
     const val TARGET_SDK = 36
-    const val VERSION_CODE = 2
-    const val VERSION_NAME = "1.0.1"
+    const val VERSION_CODE = 3
+    const val VERSION_NAME = "1.0.2"
     const val TEST_INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
     val sourceCompatibility = JavaVersion.VERSION_17
     val targetCompatibility = JavaVersion.VERSION_17
